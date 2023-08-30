2657f2c5c634acd0c1d5491b20eb27a8ed91ad0f
takaki <sabamiso2430@gmail.com>
2023-08-30 22:54:13 +0900

    deploy

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const e = require("express");
+require("dotenv").config();
+
+const PORT = 5000;
+
+//ルーティング設定
+app.use("/api/v1/tasks", taskRoute);
+
+//データベースと接続設定
+const start = async () => {
+    try {
+     await connectDB(process.env.MONGO_URL);
+        app.listen(PORT, console.log("サーバーが起動しました。"));
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+start();
+
---
+++ controllers/tasks.js
@@ -0,0 +1,27 @@
+const getAllTasks = (req, res) => {
+    res.send("タスクをすべて取得しました");
+};
+
+const createTask = (req, res) => {
+    res.send("タスクを新規作成しました");
+};
+
+const getSingleTask = (req, res) => {
+    res.send("あるタスクを取得しました");
+};
+
+const updateTask = (req, res) => {
+    res.send("あるタスクを更新しました");
+};
+
+const deleteTask = (req, res) => {
+    res.send("あるタスクを削除しました");
+};
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getSingleTask,
+    updateTask,
+    deleteTask,
+};
